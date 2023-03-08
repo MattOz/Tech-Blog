1a0e31d1bca59a135b0bbdf79960d450ed15f25f
Matt Osborne <MattJOzzy@gmail.com>
2023-03-08 17:43:11 -0600

    created file structure and seed data

---
+++ seeds/seed.js
@@ -0,0 +1,29 @@
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+const postData = require('./postData.json');
+const userData = require('./userData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+
+    await Post.bulkCreate(postData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await User.bulkCreate(userData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    await Comment.bulkCreate(commentData, {
+        individualHooks: true,
+        returning: true,
+    });
+
+    process.exit(0);
+};
+
+seedDatabase();
